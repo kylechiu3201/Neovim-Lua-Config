517017bb3a5433553705ac9fdfaab32a9670bae1
Kyle Chiu <kylechiu3201@gmail.com>
2026-04-03 12:15:32 -0500

    comment plugin

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -7,4 +7,11 @@ return {
         -- use opts = {} for passing setup options
         -- this is equivalent to setup({}) function
     },
+    -- comment plugin (in addition to Neovim built-in commenting)
+    {
+        'numToStr/Comment.nvim',
+        opts = {
+            -- add any options here
+        }
+    },
 }
