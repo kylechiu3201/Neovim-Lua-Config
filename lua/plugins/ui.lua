3365ed065298c4b1177425cd692bdfbfba6f7df6
Kyle Chiu <kylechiu3201@gmail.com>
2026-05-05 16:34:41 -0500

    use more compatible remote ssh plugin

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -800,17 +800,6 @@ local plugins = {
             vim.api.nvim_set_hl(0, "RainbowDelimiterCyan", { fg="#a5d6ff" })
         end
     },
-    -- Remote SSH
-    {
-       "amitds1997/remote-nvim.nvim",
-       version = "*", -- Pin to GitHub releases
-       dependencies = {
-           "nvim-lua/plenary.nvim", -- For standard functions
-           "MunifTanjim/nui.nvim", -- To build the plugin UI
-           "nvim-telescope/telescope.nvim", -- For picking b/w different remote methods
-       },
-       config = true,
-    },
 }
 
 
